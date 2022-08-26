ed7d45e2fd278c48da2fd4b262470959c5a86b4d
henriquesdias <henrique.dias1915@gmail.com>
2022-08-26 01:45:24 -0300

    the requisition for 'sign-up' route it's working. The message 'ok' is send for the front-end

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
+server.use(express.json());
+
+const tweets = [];
+const users = [];
+
+server.get("/tweets", (req, res) => {
+  const infoUsers = [...users];
+  infoUsers.reverse();
+  res.send(infoUsers.filter((e, index) => index <= 9));
+});
+
+server.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send(users);
+});
+server.listen(5000, () => {
+  console.log("Listening on port 5000");
+});
