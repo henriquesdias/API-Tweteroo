58a4dba44aaae03e2b00e33ad539c7185fe71ab9
henriquesdias <henrique.dias1915@gmail.com>
2022-08-29 03:28:05 -0300

    the query string 'page' is disponible. When the user clicks on button 'carregar mais' the requisition for the route 'tweet/?page' is made, showing the correct tweets

--- app.js
+++ app.js
@@ -9,12 +9,20 @@ const users = [];
 
 server.get("/tweets", (req, res) => {
   const infoTweets = [...tweets];
-  infoTweets.reverse();
-  infoTweets.forEach(
-    (elem) =>
-      (elem.avatar = users.find((e) => e.username === elem.username).avatar)
-  );
-  res.send(infoTweets.filter((e, index) => index <= 9));
+  const page = req.query.page;
+  if (page >= 1) {
+    infoTweets.reverse();
+    infoTweets.forEach(
+      (elem) =>
+        (elem.avatar = users.find((e) => e.username === elem.username).avatar)
+    );
+    return res.send(
+      infoTweets.filter(
+        (e, index) => index >= `${page - 1}0` && index < `${page}0`
+      )
+    );
+  }
+  res.send("Informe uma paǵina válida!").status(400);
 });
 server.get("/tweets/:USERNAME", (req, res) => {
   const infoTweets = [...tweets].filter(
@@ -34,7 +42,7 @@ server.post("/sign-up", (req, res) => {
     return res.status(400).send("Todos os campos são obrigatórios");
   }
   users.push(user);
-  return res.status(201).send("OK");
+  res.status(201).send("OK");
 });
 server.post("/tweets", (req, res) => {
   const user = req.headers.user;
@@ -42,8 +50,8 @@ server.post("/tweets", (req, res) => {
   if (!user || !tweet) {
     return res.status(400).send("Todos os campos são obrigatórios");
   }
-  tweets.push(tweet);
-  return res.send("OK").status(201);
+  tweets.push({ username: user, tweet: tweet });
+  res.status(201).send("OK");
 });
 server.listen(5000, () => {
   console.log("Listening on port 5000");
